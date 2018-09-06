6208656dfdfe385f6675216a7557b4de5b9d7745
YinYuWu <z5135190@student.unsw.edu.au>
2018-09-06 22:30:43 +1000

    add more item interface

---
+++ src/project/Player.java
@@ -0,0 +1,26 @@
+package project;
+
+public class Player extends Character {
+
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean useItem(Item it) {
+		// TODO
+		return false;
+	}
+	
+	public boolean pickUpItem(Item it) {
+		// TODO
+		return false;
+	}
+	
+}
---
+++ src/project/WeaponBehaviour.java
@@ -0,0 +1,7 @@
+package project;
+
+public interface WeaponBehaviour extends ItemBehaviour{
+
+	public boolean attack();
+	
+}
