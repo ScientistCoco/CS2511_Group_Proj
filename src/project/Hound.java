4f8ac89c29069d5c13e04b10c203f38ab337085e
Tong Zheng <z5142003@student.unsw.edu.au>
2018-09-11 20:29:56 +1000

    added Hound, not completed

---
+++ src/project/Hound.java
@@ -0,0 +1,29 @@
+package project;
+
+public class Hound extends Enemy {
+	public Hound(Board board) {
+		super(board);
+	}
+
+	@Override
+	public void updateMove(Player player) {
+		int x = player.getXCoordinate();
+		int y = player.getYCoordinate();
+		
+		
+	}
+
+	private boolean moveX(int x) {
+		if() {
+			return;
+		}else {
+			return;
+		}
+	}
+	
+	private boolean moveY(int y) {
+		
+	}
+	
+	
+}
