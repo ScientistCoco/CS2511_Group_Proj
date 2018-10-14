85f483b96b3c258d56b528edbe555022cb84d3c4
YinYuWu <z5135190@student.unsw.edu.au>
2018-10-14 14:01:11 +1100

    add description for items

--- src/visuals/DesignController.java
+++ src/visuals/DesignController.java
@@ -1,9 +1,13 @@
 package visuals;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import other.Board;
 
@@ -31,6 +35,9 @@ public class DesignController {
 	private int itemsPaneRow = 5;
 	private int itemsPaneCol = 3;
 	private ArrayList<Item> allitems;
+	@FXML Label itemName;
+	@FXML AnchorPane itemDescriptionPane;
+	@FXML Text itemDescription;
 	
 	public DesignController(Stage s) {
 		this.board = new Board();
@@ -52,7 +59,7 @@ public class DesignController {
 
 	@FXML
 	public void initialize() {
-		// init grid
+		// init player grid
 		for (int i = 0; i < colSize; i ++) {
 			for (int j = 0; j < rowSize; j ++ ) {
 				grid.add(board.getFloor(i, j), i, j);		
@@ -65,7 +72,7 @@ public class DesignController {
 					System.out.println(i);
 					System.out.println(j);
 					Item it = allitems.get(itemsPaneCol*j + i);
-					System.out.println(it.getItemName());
+					//System.out.println(it.getItemName());
 					itemsStackPane[i][j] = new StackPane();
 					itemsStackPane[i][j].getChildren().add(0, it.getEntityIcon());
 					itemsStackPane[i][j].getStylesheets().add("/visuals/application.css");
@@ -74,6 +81,45 @@ public class DesignController {
 				}
 			}
 		}
+		
+		// when mouse entered each item, it shows description
+		itemsGrid.getChildren().forEach((item) -> {
+			item.addEventFilter(MouseEvent.MOUSE_ENTERED, event -> {
+				int row = itemsGrid.getRowIndex(item);
+				int col = itemsGrid.getColumnIndex(item);
+				showDescription(col, row);
+			});
+		});
+		itemsGrid.getChildren().forEach((item) -> {
+			item.addEventFilter(MouseEvent.MOUSE_EXITED, event -> {
+				int row = itemsGrid.getRowIndex(item);
+				int col = itemsGrid.getColumnIndex(item);
+				hideDescription();
+			});
+		});
+	}
+	
+	public void showDescription(int col, int row) {
+		if (itemsStackPane[col][row].getChildren().size() != 0) {
+			Item it = this.findItemByImage((ImageView) itemsStackPane[col][row].getChildren().get(0));
+			itemName.setText(it.getItemName());
+			itemDescription.setText(it.getDescription());
+			itemDescriptionPane.setOpacity(1);
+			
+		}
+	}
+	
+	public void hideDescription() {
+		itemDescriptionPane.setOpacity(0);
+	}
+	
+	public Item findItemByImage(ImageView image) {
+		for (Item it : allitems) {
+			if (it.getEntityIcon().equals(image)) {
+				return it;
+			}
+		}
+		return null;
 	}
 
 }
